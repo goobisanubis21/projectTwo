95afa630b816b1f7af011615efdc8ff6f9ce1e32
Andrea <65364798+beandrea@users.noreply.github.com>
2020-12-12 13:54:27 -0500

    eslint structuring

--- routes/score-api-routes.js
+++ routes/score-api-routes.js
@@ -2,21 +2,20 @@ var db = require("../models");
 const { where } = require("sequelize/types");
 
 module.exports = function (app) {
-    app.get("/api/game/", function (req, res) {
-        db.Score.findAll({}).then(function (data) {
-            res.json(data);
-        });
+  app.get("/api/game/", function (req, res) {
+    db.Score.findAll({}).then(function (data) {
+      res.json(data);
     });
-
-    app.get("/api/game/:userId", function (req, res) {
-        db.Score.findAll({
-            where: {
-                UserId: req.params.userId
-            }
-        }).then(function (data) {
-            res.json(data);
-        });
+  });
+  app.get("/api/game/:userId", function (req, res) {
+    db.Score.findAll({
+      where: {
+        UserId: req.params.userId
+      }
+    }).then(function (data) {
+      res.json(data);
     });
+  });
 
     app.post("/api/game/:userId", function (req, res) {
         db.Score.create({
