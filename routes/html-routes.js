b7acc97d9a738d52e33cd03e0495ae2c7d4c1228
David Bleiweiss <dbleiweiss123@gmail.com>
2020-12-12 11:42:53 -0500

    added routes for serving html files

---
+++ routes/html-routes.js
@@ -0,0 +1,21 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  app.get("/signup", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/highscore", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/highscore.html"));
+  });
+
+};
