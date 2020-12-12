45b553ccfc9c65f4c68adb4752cfd3fe3598a36b
Andrea <65364798+beandrea@users.noreply.github.com>
2020-12-12 13:59:03 -0500

    more eslint structuring

--- routes/score-api-routes.js
+++ routes/score-api-routes.js
@@ -1,31 +1,29 @@
 var db = require("../models");
-const { where } = require("sequelize/types");
+//const { where } = require("sequelize/types");
 
 module.exports = function (app) {
-  app.get("/api/game/", function (req, res) {
-    db.Score.findAll({}).then(function (data) {
-      res.json(data);
+    app.get("/api/game/", function (req, res) {
+        db.Score.findAll({}).then(function (data) {
+            res.json(data);
+        });
     });
-  });
-  app.get("/api/game/:userId", function (req, res) {
-    db.Score.findAll({
-      where: {
-        UserId: req.params.userId
-      }
-    }).then(function (data) {
-      res.json(data);
+    app.get("/api/game/:userId", function (req, res) {
+        db.Score.findAll({
+            where: {
+                UserId: req.params.userId
+            }
+        }).then(function (data) {
+            res.json(data);
+        });
     });
-  });
-
     app.post("/api/game/:userId", function (req, res) {
         db.Score.create({
-            socre: req.body.score,
+            score: req.body.score,
             UserId: req.params.userId
         }).then(function (data) {
             res.json(data);
         });
-    })
-
+    });
     app.delete("/api/game/:id", function (req, res) {
         db.Score.destroy({
             where: {
@@ -34,7 +32,5 @@ module.exports = function (app) {
         }).then(function (data) {
             res.json(data);
         });
-    })
-
-
+    });
 };
