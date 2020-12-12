c6e7626310c459f313bd6a706ee4d703fd875869
David Bleiweiss <dbleiweiss123@gmail.com>
2020-12-12 11:42:31 -0500

    added routes for score db

---
+++ routes/score-api-routes.js
@@ -0,0 +1,41 @@
+var db = require("../models");
+const { where } = require("sequelize/types");
+
+module.exports = function (app) {
+    app.get("/api/game/", function (req, res) {
+        db.Score.findAll({}).then(function (data) {
+            res.json(data);
+        });
+    });
+
+    app.get("/api/game/:userId", function (req, res) {
+        db.Score.findAll({
+            where: {
+                UserId: req.params.userId
+            }
+        }).then(function (data) {
+            res.json(data);
+        });
+    });
+
+    app.post("/api/game/:userId", function (req, res) {
+        db.Score.create({
+            socre: req.body.score,
+            UserId: req.params.userId
+        }).then(function (data) {
+            res.json(data);
+        });
+    })
+
+    app.delete("/api/game/:id", function (req, res) {
+        db.Score.destroy({
+            where: {
+                id: req.paramas.id
+            }
+        }).then(function (data) {
+            res.json(data);
+        });
+    })
+
+
+};
