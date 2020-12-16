a037aebed20a70823c41fcc294dd49b70ac2f1c2
Anthony Parrino <anthony21parrino21@gmail.com>
2020-12-15 21:49:06 -0500

    added autheticated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,7 @@ var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-    app.get("/", function(req, res) {
+    app.get("/", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
     app.get("/login", function(req, res) {
@@ -12,9 +12,10 @@ module.exports = function(app) {
     app.get("/signup", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
-    app.get("/highscore", function(req, res) {
+    app.get("/highscore", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/highscore.html"));
     });
-    app.get("/game", function(req, res) {
+    app.get("/game", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/hangman.html"));
     });
+}
