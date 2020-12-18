5269e1e169196a1b5b83a44d28554c2c6e61bf33
Andrea <65364798+beandrea@users.noreply.github.com>
2020-12-17 21:15:54 -0500

    buttons work now

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,28 +1,25 @@
 $(document).ready(function () {
+    var userName = $("#Username");
+    var passWord = $("#Password");
+    var conFirm = $("#Confirm");
     $("#btnSignUp").on("click", function (e) {
         e.preventDefault;
-        var userName = $("#Username").val().trim();
-        var passWord = $("#Password").val().trim();
-        var conFirm = $("#Confirm").val().trim();
-        console.log(userName);
-        console.log(passWord);
         userData = {
-            username: userName,
-            password: passWord
+            username: userName.val().trim(),
+            password: passWord.val().trim()
         };
-        
-        var {name, pass} = userData;
 
-        if(!name || !pass || pass != conFirm ){
+        if(!userData.username || !userData.password || userData.password != conFirm.val().trim()){
             return;
         }
 
-        postToServer(name, pass);
-        name.val("");
-        pass.val("");
+        postToServer(userData.username, userData.password);
+        userName.val("");
+        passWord.val("");
     });
 
     function postToServer(name, password){
+        console.log("hello world, i eat ass")
         $.post("/api/signup", {
             email: name,
             password: password
