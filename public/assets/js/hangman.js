fff09e01ada793c46b51f8964d93fc87afd8e09a
David Bleiweiss <dbleiweiss123@gmail.com>
2020-12-19 11:34:22 -0500

    route to get user data on hangman game

--- public/assets/js/hangman.js
+++ public/assets/js/hangman.js
@@ -1,45 +1,61 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var clicked = []
     var wordArr = []
     var numWrong = 0;
     var word
     var score = 0;
 
-    $.get('/api/get-word/', function(data) {
+    var userData;
+
+    //
+    $.get('/api/get-word/', function (data) {
         word = data.toUpperCase()
         console.log(word)
-        
+
         wordArr = [...word]
 
         var wordLen = word.length
-        
-        for(let i = 0; i < wordLen; i++) {
+
+        for (let i = 0; i < wordLen; i++) {
             $("#wordContainer").append(
                 `<div id="letter${i}" class="incorrectLetter"></div>`
             )
         }
-    }) 
+    })
 
-    $(".letterButton").on('click', function(){
+    var userId = 1;
+
+    //Get user data on page load
+    $.get('/api/users/' + userId, function (data) {
+        userData = data;
+        console.log(data)
+    });
+
+    var winStreak = userData.winStreak;
+    var combineScore = userData.combineScore;
+    var currWinStreak = userData.currWinStreak;
+    var currCombineScore = userData.currCombineScore;
+
+    $(".letterButton").on('click', function () {
         $(this).prop('disabled', true)
         var letterClicked = $(this).attr('data-letter')
         clicked.push(letterClicked)
 
-        if(wordArr.includes(letterClicked)) {
+        if (wordArr.includes(letterClicked)) {
             var letterIndex = []
 
-            wordArr.forEach((e,i) => {
+            wordArr.forEach((e, i) => {
                 if (letterClicked == e) {
                     letterIndex.push(i)
                 }
             });
-            
+
             letterIndex.forEach(e => {
                 $("#letter" + e).text(letterClicked)
                 $("#letter" + e).removeClass("incorrectLetter")
             });
 
-            if(document.getElementsByClassName('incorrectLetter').length == 0) {
+            if (document.getElementsByClassName('incorrectLetter').length == 0) {
                 complete()
             }
 
@@ -51,7 +67,7 @@ $(document).ready(function(){
         }
     })
 
-    $("#guessButton").on('click',function() {
+    $("#guessButton").on('click', function () {
         let guess = prompt("what is your guess")
 
         if (word == guess.toUpperCase()) {
@@ -62,72 +78,102 @@ $(document).ready(function(){
         }
     });
 
-    $("#cancelButton").on('click',function() {
-        if(confirm("are your sure you want to quit?")) {
+    $("#cancelButton").on('click', function () {
+        if (confirm("are your sure you want to quit?")) {
             window.location.href = '/';
         }
     });
 
-    $("#tryAgain").on('click',function() {
+    $("#tryAgain").on('click', function () {
         window.location.href = '/game';
     });
 
-    $("#leaveGame").on('click',function() {
+    $("#leaveGame").on('click', function () {
         window.location.href = '/';
     });
-    
+
+
+
+
+    function wrongLetter(numWrong) {
+
+        switch (numWrong) {
+
+            case 1:
+                $("#gameImage").attr("src", "./assets/images/hangman/oneWrong.png");
+                break;
+
+            case 2:
+                $("#gameImage").attr("src", "./assets/images/hangman/twoWrong.png");
+                break;
+
+            case 3:
+                $("#gameImage").attr("src", "./assets/images/hangman/threeWrong.png");
+                break;
+
+            case 4:
+                $("#gameImage").attr("src", "./assets/images/hangman/fourWrong.png");
+                break;
+
+            case 5:
+                $("#gameImage").attr("src", "./assets/images/hangman/fiveWrong.png");
+                break;
+
+            case 6:
+                $("#gameImage").attr("src", "./assets/images/hangman/sixWrong.png");
+                break;
+        }
+
+        if (numWrong == 6) {
+            //logic to end game
+            score = 0;
+            $('#points').text(score);
+            complete()
+        }
+        else {
+            let currNum = $('#points').text()
+            score = parseInt(currNum) - 10;
+            $('#points').text(score);
+        }
+    }
+
+    function disableAll() {
+        $(".gameButton").prop('disabled', true)
+    }
+
+    function complete() {
+        disableAll()
+
+        $("#finishModalContainer").removeClass('hidden')
+        $("#finalScore").text(score);
+
+        if (score == 0) {
+            resetWin()
+        }
+        else {
+
+            currWinStreak++;
+
+            $("#winStreak").text(currWinStreak);
+            $("#combineScore").text(currCombineScore);
+
+        }
+
+    }
+
+    function updateWin() {
+
+    }
+
+    function resetWin() {
+        $("#winStreak").text("0");
+        $("#combineScore").text("0");
+
+        // $.put("/api/user-score/" + userData.id + "/0/0")
+    }
+
+    function newBest() {
+
+    }
+
 });
-
-
-function wrongLetter(numWrong) {
-    
-    switch (numWrong) {
-
-        case 1:
-            $("#gameImage").attr("src", "./assets/images/hangman/oneWrong.png");
-            break;
-
-        case 2:
-            $("#gameImage").attr("src", "./assets/images/hangman/twoWrong.png");
-            break;
-
-        case 3:
-            $("#gameImage").attr("src", "./assets/images/hangman/threeWrong.png");
-            break;
-
-        case 4:
-            $("#gameImage").attr("src", "./assets/images/hangman/fourWrong.png");
-            break;
-
-        case 5:
-            $("#gameImage").attr("src", "./assets/images/hangman/fiveWrong.png");
-            break;
-
-        case 6:
-            $("#gameImage").attr("src", "./assets/images/hangman/sixWrong.png");
-            break;
-    }
-
-    if (numWrong == 6) {
-        //logic to end game
-        score = 0;
-        $('#points').text(score);
-        complete()
-    }
-    else {
-        let currNum = $('#points').text()
-        score = parseInt(currNum) - 10;
-        $('#points').text(score);
-    }
-}
-
-function disableAll() {
-    $(".gameButton").prop('disabled', true)
-}
-
-function complete() {
-    disableAll()
-    $("#finishModalContainer").removeClass('hidden')
-    $("#finalScore").text(score);
-    //add function to push final score
-}
