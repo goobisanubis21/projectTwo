4d68ab35f22a4fc031b00cf4efbca04576104ec2
Andrea <65364798+beandrea@users.noreply.github.com>
2020-12-19 14:07:42 -0500

    god rest ye merry gentlefolk

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,25 @@
 $(document).ready(function (){
     const statusArea = $("#statuses");
+
+    $.get("/api/status/").then(function (data) {
+        console.log(data);
+        for(let i = 0; i < data.length; i++) {
+            let card = $("<div>").addClass("card");
+            let cardBody = $("<div>").addClass("card-body");
+            card.append(cardBody);
+
+            let cardTitle = $("<h5>").addClass("card-title");
+            cardTitle.text(data[i].UserId);
+            cardBody.append(cardTitle);
+
+            let cardText = $("<p>").addClass("card-text");
+            cardText.text(data[i].text);
+            cardBody.append(cardText);
+
+            statusArea.prepend(card);
+        }
+    });
+
     var userId;
 
     $.get("/api/user/", function (data){
@@ -25,11 +45,21 @@ $(document).ready(function (){
                         let cardText = $("<p>").addClass("card-text");
                         cardText.text(data[i].text);
                         cardBody.append(cardText);
-    
+
+                        if(data[i].UserId === userId){
+                            let delBtn = $("<button>").addClass("btn btn-warning");
+                            delBtn.attr("type", "button");
+                            cardBody.append(delBtn);
+                        }
+
                         statusArea.prepend(card);
                     }
                 });
             });
+        });
+
+        $(".btn-warning").on("click", function (){
+            
         });
     });
 });
