cafdd636575c0855bc6599147b4f6ab45eac43ba
Anthony Parrino <anthony21parrino21@gmail.com>
2020-12-19 21:30:43 -0500

    made changes to html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,13 +12,10 @@ module.exports = function(app) {
     app.get("/signup", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
-    // app.get("/highscore", isAuthenticated, function(req, res) {
-    app.get("/highscore", function(req, res) {
+    app.get("/highscore", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/highscore.html"));
     });
-    // app.get("/game", isAuthenticated, function(req, res) {
-    app.get("/game", function(req, res) {
-
+    app.get("/game", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/hangman.html"));
     });
 }
