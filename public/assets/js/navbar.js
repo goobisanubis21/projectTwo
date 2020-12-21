ee9e2d49565fc84965617586422716b551021534
Anthony Parrino <anthony21parrino21@gmail.com>
2020-12-21 10:52:23 -0500

    added new nav bar to all pages and gave them links

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,55 +1,24 @@
 $(document).ready(function () {
-    $("body").prepend(` 
-    
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="zIndex">
-<div class="container-fluid">
-    <a class="navbar-brand" href="#">HangdOut</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-4">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/highscore">Highscores</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/game">Play Hangman</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/login">Logout</a>
-            </li>
-        </ul>
-        
-         
-    </div>
-    
-</div>
-</nav>
-
+    $("body").prepend(`
 <div class="sidenav">
-        <a href="">
-            <h1> HangdOut </h1></a>
-        <a href="">Dunno</a>
-        <a href="">Highscores</a>
-        <a href="">Games</a>
-        <a href="">Logout</a>
+        <a href="/">
+            <h1 id="navTitle"> HangdOut </h1></a>
+        <a href="/">Home</a>
+        <a href="/highscore">Highscores</a>
+        <a href="/game">Play Hangman</a>
+        <a href="/login">Logout</a>
         <button id = "deleteMe">Delete Account</button>
     </div>`);
 
-    $("#deleteMe").on("click", function() {
+    $("#deleteMe").on("click", function () {
         var userId;
-        $.get("/api/user", function(data) {
+        $.get("/api/user", function (data) {
             userId = data.id;
-        }).then(function() {
-            $.ajax ({
+        }).then(function () {
+            $.ajax({
                 method: "DELETE",
                 url: "/api/users/" + userId
-            }).then(function() {
+            }).then(function () {
                 window.replace("/login")
             })
         })
