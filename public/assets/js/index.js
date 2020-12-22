83db624f353085cf706bc7770c65675422f5f6ff
Andrea <65364798+beandrea@users.noreply.github.com>
2020-12-22 13:13:46 -0500

    why is this id not adding

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,12 @@
-$(document).ready(function (){
+$(document).ready(function () {
     const statusArea = $("#statuses");
 
     $.get("/api/status/").then(function (data) {
         console.log(data);
-        for(let i = 0; i < data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
             let card = $("<div>").addClass("card");
+            card.attr("id", "centerThis");
+
             let cardBody = $("<div>").addClass("card-body");
             card.append(cardBody);
 
@@ -22,10 +24,10 @@ $(document).ready(function (){
 
     var userId;
 
-    $.get("/api/user/", function (data){
+    $.get("/api/user/", function (data) {
         userId = data.id;
     }).then(function () {
-        $("#btnSubmit").on("click", function() {
+        $("#btnSubmit").on("click", function () {
             $.post("/api/status/", {
                 text: $("#statusUpdate").val(),
                 id: userId
@@ -33,20 +35,20 @@ $(document).ready(function (){
                 statusArea.empty();
                 $.get("/api/status/").then(function (data) {
                     console.log(data);
-                    for(let i = 0; i < data.length; i++) {
+                    for (let i = 0; i < data.length; i++) {
                         let card = $("<div>").addClass("card");
                         let cardBody = $("<div>").addClass("card-body");
                         card.append(cardBody);
-    
+
                         let cardTitle = $("<h5>").addClass("card-title");
                         cardTitle.text(data[i].User.email);
                         cardBody.append(cardTitle);
-    
+
                         let cardText = $("<p>").addClass("card-text");
                         cardText.text(data[i].text);
                         cardBody.append(cardText);
 
-                        if(data[i].UserId === userId){
+                        if (data[i].UserId === userId) {
                             let delBtn = $("<button>").addClass("btn btn-warning");
                             delBtn.attr("type", "button");
                             delBtn.text("Delete")
@@ -59,8 +61,8 @@ $(document).ready(function (){
             });
         });
 
-        $(".btn-warning").on("click", function (){
-            
+        $(".btn-warning").on("click", function () {
+
         });
     });
 });
